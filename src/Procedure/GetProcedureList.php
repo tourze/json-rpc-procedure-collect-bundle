0126284ff984eeae0c92244a6534b35d952fb510
github-ci <you@example.com>
2025-04-27 17:29:05 +0000

    feat(procedure): 添加获取过程列表的JSON-RPC过程

---
+++ src/Procedure/GetProcedureList.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tourze\JsonRPCProcedureCollectBundle\Procedure;
+
+use Tourze\JsonRPC\Core\Attribute\MethodExpose;
+use Tourze\JsonRPC\Core\Procedure\BaseProcedure;
+use Tourze\JsonRPCProcedureCollectBundle\Service\NameCollector;
+
+#[MethodExpose('GetProcedureList')]
+class GetProcedureList extends BaseProcedure
+{
+    public function __construct(private readonly NameCollector $collector)
+    {
+    }
+
+    public function execute(): array
+    {
+        return $this->collector->getProcedures();
+    }
+}
